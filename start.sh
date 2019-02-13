490189fa4c11daee2b512bcafeecdd4825608d5a
Frank Martens <martensfwm@gmail.com>
2019-02-13 01:37:41 -0800

    blah

--- start.sh
+++ start.sh
@@ -14,20 +14,19 @@ fi
 
 MA_PROPERTIES="-Dappdynamics.controller.hostName=${CONTROLLER_HOST}"
 MA_PROPERTIES+=" -Dappdynamics.controller.port=${CONTROLLER_PORT}"
+MA_PROPERTIES+=" -Dappdynamics.agent.applicationName=${APPLICATION_NAME}"
 MA_PROPERTIES+=" -Dappdynamics.agent.accountName=${ACCOUNT_NAME}"
 MA_PROPERTIES+=" -Dappdynamics.agent.accountAccessKey=${ACCOUNT_ACCESS_KEY}"
 MA_PROPERTIES+=" -Dappdynamics.controller.ssl.enabled=${CONTROLLER_SSL_ENABLED}"
-MA_PROPERTIES+=" -Dappdynamics.agent.applicationName=${APPLICATION_NAME}"
-MA_PROPERTIES+=" -Dappdynamics.agent.tierName=${TIER_NAME}"
-MA_PROPERTIES+=" -Dappdynamics.agent.nodeName=${APPLICATION_NAME}_k8sclstrnode"
 MA_PROPERTIES+=" -Dappdynamics.machine.agent.hierarchyPath=SVM-${HOSTNAME}"
-MA_PROPERTIES+=" -Dappdynamics.sim.enabled=${SIM_ENABLED} -Dappdynamics.docker.enabled=false"
-
-
-#MA_PROPERTIES+=" -Dappdynamics.docker.container.containerIdAsHostId.enabled=${ENABLE_CONTAINERIDASHOSTID}"
-
+MA_PROPERTIES+=" -Dappdynamics.sim.enabled=false"
+MA_PROPERTIES+=" -Dappdynamics.docker.enabled=false"
+MA_PROPERTIES+=" -Dappdynamics.agent.tierName=${TIER_NAME}"
+MA_PROPERTIES+=" -Dappdynamics.agent.nodeName=${TIER_NAME}_k8sclstrnode1"
 if [ "x$UNIQUE_HOSTID" != "x" ]; then
     MA_PROPERTIES+=" -Dappdynamics.agent.uniqueHostId=${UNIQUE_HOSTID}"
+else
+    MA_PROPERTIES+=" -Dappdynamics.agent.uniqueHostId=k8s-${APPLICATION_NAME}"
 fi
 
 if [ "x$PROXY_HOST" != "x" ]; then
@@ -46,10 +45,12 @@ if [ "x$PROXY_PASS" != "x" ]; then
     MA_PROPERTIES+=" -Dappdynamics.http.proxyPasswordFile=${PROXY_PASS}"
 fi
 
-
-
 if [ "x$METRIC_LIMIT" != "x" ]; then
     MA_PROPERTIES+=" -Dappdynamics.agent.maxMetrics=${METRIC_LIMIT}"
+fi
+
+if [ "x$MACHINE_AGENT_PROPERTIES" != "x" ]; then
+    MA_PROPERTIES+=" ${MACHINE_AGENT_PROPERTIES}"
 fi
 
 # Start Machine Agent
